63703e83135129ea0970c30fe22886dba227a774
gauravk1302 <gauravsaini77069@gmail.com>
2025-07-25 19:58:03 +0530

    save data and generate HTML for delivery options

--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -1,3 +1,7 @@
 export function formatCurrency(priceCents){
     return (priceCents/100).toFixed(2);
 }
+
+export default formatCurrency;
+
+// now we can import without using the curly bracket and also each file can have only one export default 
