2fff0e34c2330f5475b03383dfac17ddd3573ab3
gauravk1302 <gauravsaini77069@gmail.com>
2025-07-31 15:06:59 +0530

    Convert from promises to async  await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,47 +5,60 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts, loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 
-Promise.all([
-loadProductsFetch(),
-  new Promise((resolve) => {
-      loadCart(() => {
-        resolve();
-      });
-    })
 
-]).then((values) =>{
-    console.log(values);
-      renderOrderSummary();
-    renderPaymentSummary();
-}) ;
+
+async function loadPage() {
+ await loadProductsFetch();
+  
+ 
+ const value = await new Promise((resolve) => {
+    loadCart(() => {
+      resolve('value3');
+    });
+  });
+
+  renderOrderSummary();
+  renderPaymentSummary();
+}
+loadPage();
+
+
+
+
+// Promise.all([
+// loadProductsFetch(),
+//   new Promise((resolve) => {
+//       loadCart(() => {
+//         resolve();
+//       });
+//     })
+
+// ]).then((values) =>{
+//     console.log(values);
+//       renderOrderSummary();
+//     renderPaymentSummary();
+// }) ;
 
 // new Promise((resolve) => {
 //   loadProducts(() => {
 //     resolve('value1');
 //   });
 
-
-
 //   }).then((value) => {
 //     console.log(value);
-    
+
 //     return new Promise((resolve) => {
 //       loadCart(() => {
 //         resolve();
 //       });
 //     });
-  
-
-  
 
 //   }).then(() => {
 //     renderOrderSummary();
 //     renderPaymentSummary();
 //   });
 
-
-
-  // loadProducts(() => {
+// loadProducts(() => {
 //     loadCart(() =>{
 //         renderOrderSummary();
 //         renderPaymentSummary();
